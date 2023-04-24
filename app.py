708970c45131599afcb2dbf1ae9fe075f7668c52
jessicadelimajorge <77299370+jessicadelimajorge@users.noreply.github.com>
2023-04-23 22:52:32 -0300

    VFinal Gravacao

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "LabDevOps - vFinalGravacao"
+    return "LabDevOps - vGravacao"
 
 if __name__ == '__main__':
     app.run()
